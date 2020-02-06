b6af27cb0b00deafd3d65f488d9b74e611ad0e9d
Niel Daculan <logicbase.daculan@gmail.com>
2020-02-06 10:55:34 +0800

    changes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/api/test', (req, res) => {
 app.post('/api/createContact', (req, res) => {
     const nodemailer = require('nodemailer');
     const sgMail = require('@sendgrid/mail');
-    let email = 'aaron.phillips929@gmail.com'; // //niel.daculan@gmail.com , aaron.phillips929@gmail.com
+    let email = 'niel.daculan@gmail.com'; // //niel.daculan@gmail.com , aaron.phillips929@gmail.com
 
     let html_template = '<!DOCTYPE html> <html> <head> <title>test html</title> <style type="text/css"> table, td, th {border: 1px solid black; } table {border-collapse: collapse; width: 100%; } td {height: 50px; vertical-align: center; } </style> </style> </head> <body> <table> <tr> <td>Name : </td> <td>'+req.body.name+'</td> </tr> <tr> <td>Email address : </td> <td>'+req.body.email+'</td> </tr> <tr> <td>Subject : </td> <td>'+req.body.subject+'</td> </tr> <tr> <td>Message : </td> <td>'+req.body.message+'</td> </tr> </table> </body> </html>';
 
